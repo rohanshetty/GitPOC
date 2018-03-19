70e9f0a31f4e5476fe5202767f1a8906adf31b56
Rohan <rvs1590@gmail.com>
2018-03-19 17:07:54 -0400

    Developer 1: Change 2

--- GitPOC/Program.cs
+++ GitPOC/Program.cs
@@ -8,6 +8,7 @@ namespace GitPOC
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Developer 1: Change 1");
+            Console.WriteLine("Developer 1: Change 2");
         }
     }
 }
